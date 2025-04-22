177b6ffd763b444412f68db8354f002259be6f4f
gunshipac130 <chawis_49@hotmail.com>
2025-04-22 16:23:27 +0700

    regex raw prefix bool to string fix for no correct hex found format cleanup

--- alex_regex.py
+++ alex_regex.py
@@ -1,33 +1,5 @@
 import re
 
-# {
-# 	"C2S_ActionRequest": "0x0283",
-# 	"C2S_ActionRequestGroundTargeted": "0x01ef",
-# 	"Common_UseOodleTcp": true,
-# 	"PatchCode": [],
-# 	"S2C_ActionEffect01": "0x01bf",
-# 	"S2C_ActionEffect08": "0x0104",
-# 	"S2C_ActionEffect16": "0x02b8",
-# 	"S2C_ActionEffect24": "0x016c",
-# 	"S2C_ActionEffect32": "0x0217",
-# 	"S2C_ActorCast": "0x007b",
-# 	"S2C_ActorControl": "0x026f",
-# 	"S2C_ActorControlSelf": "0x00b2",
-# 	"Server_IpRange": "0.0.0.0/0",
-# 	"Server_PortRange": "1-65535"
-# }
-
-# Name string holder
-C2S_ActionRequest = ""  #0
-C2S_ActionRequestGroundTargeted = ""    #1
-S2C_ActionEffect01 = "" #2
-S2C_ActionEffect08 = "" #3
-S2C_ActionEffect16 = "" #4
-S2C_ActionEffect24 = "" #5
-S2C_ActionEffect32 = "" #6
-S2C_ActorCast = ""  #7
-S2C_ActorControl = ""   #8
-S2C_ActorControlSelf = ""   #9
 HolderArray = [False, False, False, False, False, False, False, False, False, False]
 
 textfile = open('string.txt', 'r', encoding='utf8')
@@ -42,62 +14,71 @@ for line in textfile:
     line = line.rstrip()
     tempstring = ""
 
-    tempstring = re.findall('\d{4}.*IpcTypeFinder.*C2S_ActionRequest\/GroundTargeted\((.{6})\).*actionId=08ce', line)
+    tempstring = re.findall(r'\d{4}.*IpcTypeFinder.*C2S_ActionRequest\/GroundTargeted\((.{6})\).*actionId=08ce', line)
     if addExtracted(tempstring) != None :
         HolderArray[0] = addExtracted(tempstring)
 
-    tempstring = re.findall('\d{4}.*IpcTypeFinder.*S2C_ActorControlSelf\((.{6})\).*actionId=08ce', line)
+    tempstring = re.findall(r'\d{4}.*IpcTypeFinder.*S2C_ActorControlSelf\((.{6})\).*actionId=08ce', line)
     if addExtracted(tempstring) != None :
         HolderArray[9] = addExtracted(tempstring)
 
-    tempstring = re.findall('\d{4}.*IpcTypeFinder.*C2S_ActionRequest\/GroundTargeted\((.{6})\).*actionId=0df1', line)
+    tempstring = re.findall(r'\d{4}.*IpcTypeFinder.*C2S_ActionRequest\/GroundTargeted\((.{6})\).*actionId=0df1', line)
     if addExtracted(tempstring) != None :
         HolderArray[1] = addExtracted(tempstring)
 
-    tempstring = re.findall('\d{4}.*IpcTypeFinder.*S2C_ActorCast\((.{6})\).*actionId=0078', line)
+    tempstring = re.findall(r'\d{4}.*IpcTypeFinder.*S2C_ActorCast\((.{6})\).*actionId=0078', line)
     if addExtracted(tempstring) != None :
         HolderArray[7] = addExtracted(tempstring)
 
-    tempstring = re.findall('\d{4}.*IpcTypeFinder.*S2C_ActorControl\((.{6})\).*actionId=0078', line)
+    tempstring = re.findall(r'\d{4}.*IpcTypeFinder.*S2C_ActorControl\((.{6})\).*actionId=0078', line)
     if addExtracted(tempstring) != None :
         HolderArray[8] = addExtracted(tempstring)
 
-
-    
-    tempstring = re.findall('\d{4}.*IpcTypeFinder.*S2C_ActionEffect01\((.{6})\)\slength=9c', line)
+    tempstring = re.findall(r'\d{4}.*IpcTypeFinder.*S2C_ActionEffect01\((.{6})\)\slength=9c', line)
     if addExtracted(tempstring) != None :
         HolderArray[2] = addExtracted(tempstring)
     
-    tempstring = re.findall('\d{4}.*IpcTypeFinder.*S2C_ActionEffect08\((.{6})\)\slength=29c', line)
+    tempstring = re.findall(r'\d{4}.*IpcTypeFinder.*S2C_ActionEffect08\((.{6})\)\slength=29c', line)
     if addExtracted(tempstring) != None :
         HolderArray[3] = addExtracted(tempstring)
 
-    tempstring = re.findall('\d{4}.*IpcTypeFinder.*S2C_ActionEffect16\((.{6})\)\slength=4dc', line)
+    tempstring = re.findall(r'\d{4}.*IpcTypeFinder.*S2C_ActionEffect16\((.{6})\)\slength=4dc', line)
     if addExtracted(tempstring) != None :
         HolderArray[4] = addExtracted(tempstring)
     
-    tempstring = re.findall('\d{4}.*IpcTypeFinder.*S2C_ActionEffect24\((.{6})\)\slength=71c', line)
+    tempstring = re.findall(r'\d{4}.*IpcTypeFinder.*S2C_ActionEffect24\((.{6})\)\slength=71c', line)
     if addExtracted(tempstring) != None :
         HolderArray[5] = addExtracted(tempstring)
     
-    tempstring = re.findall('\d{4}.*IpcTypeFinder.*S2C_ActionEffect32\((.{6})\)\slength=95c', line)
+    tempstring = re.findall(r'\d{4}.*IpcTypeFinder.*S2C_ActionEffect32\((.{6})\)\slength=95c', line)
     if addExtracted(tempstring) != None :
         HolderArray[6] = addExtracted(tempstring)
 
+C2S_ActionRequest = str(HolderArray[0])  #0
+C2S_ActionRequestGroundTargeted = str(HolderArray[1])    #1
+S2C_ActionEffect01 = str(HolderArray[2]) #2
+S2C_ActionEffect08 = str(HolderArray[3]) #3
+S2C_ActionEffect16 = str(HolderArray[4]) #4
+S2C_ActionEffect24 = str(HolderArray[5]) #5
+S2C_ActionEffect32 = str(HolderArray[6]) #6
+S2C_ActorCast = str(HolderArray[7])  #7
+S2C_ActorControl = str(HolderArray[8])   #8
+S2C_ActorControlSelf = str(HolderArray[9])   #9
+
 print(HolderArray, "\n")
 print("{")
-print("	\"C2S_ActionRequest\": \"" + HolderArray[0]+ "\",")
-print("	\"C2S_ActionRequestGroundTargeted\": \"" + HolderArray[1]+ "\",")
+print("	\"C2S_ActionRequest\": \"" + C2S_ActionRequest + "\",")
+print("	\"C2S_ActionRequestGroundTargeted\": \"" + C2S_ActionRequestGroundTargeted + "\",")
 print("	\"Common_UseOodleTcp\": true,")
 print("	\"PatchCode\": [],")
-print("	\"S2C_ActionEffect01\": \"" + HolderArray[2]+ "\",")
-print("	\"S2C_ActionEffect08\": \"" + HolderArray[3]+ "\",")
-print("	\"S2C_ActionEffect16\": \"" + HolderArray[4]+ "\",")
-print("	\"S2C_ActionEffect24\": \"" + HolderArray[5]+ "\",")
-print("	\"S2C_ActionEffect32\": \"" + HolderArray[6]+ "\",")
-print("	\"S2C_ActorCast\": \"" + HolderArray[7]+ "\",")
-print("	\"S2C_ActorControl\": \"" + HolderArray[8]+ "\",")
-print("	\"S2C_ActorControlSelf\": \"" + HolderArray[9]+ "\",")
+print("	\"S2C_ActionEffect01\": \"" + S2C_ActionEffect01 + "\",")
+print("	\"S2C_ActionEffect08\": \"" + S2C_ActionEffect08 + "\",")
+print("	\"S2C_ActionEffect16\": \"" + S2C_ActionEffect16 + "\",")
+print("	\"S2C_ActionEffect24\": \"" + S2C_ActionEffect24 + "\",")
+print("	\"S2C_ActionEffect32\": \"" + S2C_ActionEffect32 + "\",")
+print("	\"S2C_ActorCast\": \"" + S2C_ActorCast + "\",")
+print("	\"S2C_ActorControl\": \"" + S2C_ActorControl + "\",")
+print("	\"S2C_ActorControlSelf\": \"" + S2C_ActorControlSelf + "\",")
 print("	\"Server_IpRange\": \"0.0.0.0/0\",")
 print("	\"Server_PortRange\": \"1-65535\"")
 print("}\n")
